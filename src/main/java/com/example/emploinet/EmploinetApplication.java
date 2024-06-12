87e80a4e4a0201acf70e163198d7054112185c96
Ghassen <benghazighassen12@gmail.com>
2024-06-12 14:40:03 +0100

    springboot files

---
+++ src/main/java/com/example/emploinet/EmploinetApplication.java
@@ -0,0 +1,15 @@
+package com.example.emploinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmploinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmploinetApplication.class, args);
+
+		System.out.println("hello world !");
+	}
+
+}
---
+++ src/test/java/com/example/emploinet/EmploinetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emploinet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmploinetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
